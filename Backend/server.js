eaeabc51a76dfbdc8da89c5e136a846f53f09f53
Nitik Bisht <nitikbisht62@gmail.com>
2025-06-29 23:53:44 +0530

    test action file

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,7 @@ const { verifyToken } = require('./middleware/auth');
 
 app.use(express.json());
 app.use(cors());
+
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 });
